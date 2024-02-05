3276ecd3e6dbe45832d5291071600b047908d854
AYUSH KUMAR <97211497+ayu55sh@users.noreply.github.com>
2024-02-05 07:58:20 +0530

    Add files via upload
    now se here we use a string to show how this variable the method which we want to write a code.

---
+++ B.java
@@ -0,0 +1,15 @@
+package app_java_1;
+
+public class B {
+	public static void main(String[] args) {
+		B bittu = new B();
+		String man=bittu.test();
+		System.out.println(man);
+	
+}
+	public String test() {
+		return "AYUSH";
+		
+	}
+
+}
