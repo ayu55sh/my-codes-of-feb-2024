83102e5d30c1c716e8071550233a8cf6bea021c5
AYUSH KUMAR <97211497+ayu55sh@users.noreply.github.com>
2024-02-05 08:30:30 +0530

    Add files via upload

---
+++ D.java
@@ -0,0 +1,17 @@
+package app_java_1;
+
+public class D {
+	public static void main(String[] args) {
+		D bittu = new D();
+		
+	
+		bittu.test(33);
+		
+		
+	}
+	public void test(int x) {
+		System.out.println(x);
+         
+	}
+
+}
