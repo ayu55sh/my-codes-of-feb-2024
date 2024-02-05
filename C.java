6d6d0b87040309ed05eaff2ca8536093dcd70e87
AYUSH KUMAR <97211497+ayu55sh@users.noreply.github.com>
2024-02-05 08:11:55 +0530

    Add files via upload
    now se here we use a string to show how this variable the method which we want to write a code.

---
+++ C.java
@@ -0,0 +1,14 @@
+package app_java_1;
+
+public class C {
+	public static void main(String[] args) {
+		int bittu= C.test();
+		System.out.println(C.test());
+		
+	}
+	public static  int test() {
+		return 93873;
+	}
+	
+
+}
