c1a6efbdb20d83962be9e5092284f12f348ce8a4
AYUSH KUMAR <97211497+ayu55sh@users.noreply.github.com>
2024-02-05 08:35:37 +0530

    Add files via upload

---
+++ E.java
@@ -0,0 +1,14 @@
+package app_java_1;
+
+public class E {
+	public static void main(String[] args) {
+		E bittu=new E();
+		bittu.test(8383, "AYUSH THAKUR", 847.2838f);
+		
+	}
+    public void test (int x, String y,float z) {
+    	System.out.println(x);
+    	System.out.println(y);
+    	System.out.println(z);
+    }
+}
