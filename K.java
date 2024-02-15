75ad8569a4b6d392c7506b6274d5026a586691a1
AYUSH KUMAR <97211497+ayu55sh@users.noreply.github.com>
2024-02-15 22:26:52 +0530

    now i am trying to know the diff between both of there value to

---
+++ K.java
@@ -0,0 +1,14 @@
+package app_java_1;
+
+public class K {
+	
+	public static void main(String[] args) {
+		K bittu = new K();
+		bittu.test1();
+		 
+	}
+
+	public void test1() {
+		System.out.println(34);
+	}
+}
